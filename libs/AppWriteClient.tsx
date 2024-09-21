c83203d9949ca7b8ad4b6439df1f9992ce7b7493
krishna_yadav001 <krishnayadav123345@gmail.com>
2024-09-21 16:35:00 +0530

    hi

--- libs/AppWriteClient.tsx
+++ libs/AppWriteClient.tsx
@@ -2,11 +2,14 @@ import { Account, Client, ID, Databases, Query, Storage } from 'appwrite';
 
 const client = new Client()
 
-    .setEndpoint('https://cloud.appwrite.io/v1')
-    .setProject('66ee48ac001b934cc6c1');
+    .setEndpoint(String(process.env.NEXT_PUBLIC_APPWRITE_URL))
+    .setProject(String(process.env.NEXT_PUBLIC_ENDPOINT));
 
     const account = new Account(client);
     const database = new Databases(client);
     const storage = new Storage(client);
 
 export { client , account , database, storage, Query, ID}
+
+//.setEndpoint('https://cloud.appwrite.io/v1')
+//.setProject('66ee48ac001b934cc6c1');
